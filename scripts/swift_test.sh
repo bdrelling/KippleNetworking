6a9f52e321f4e653c07cdff045efe55a9a5f22bb
Brian Drelling <brian@drelling.com>
2022-07-05 00:51:46 -0500

    Fixed deploy directories

--- scripts/swift_test.sh
+++ scripts/swift_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir $DEPLOY_DIRECTORY
+mkdir -p $DEPLOY_DIRECTORY
 
 swift test -c debug --enable-code-coverage
-cp $(swift test --show-codecov-path) $DEPLOY_DIRECTORY/codecov.json
+cp $(swift test --show-codecov-path) "${DEPLOY_DIRECTORY}/codecov.json"
