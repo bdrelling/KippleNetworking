3d50c7b33bc642c7cb697c45e9d4eee82be67883
Brian Drelling <brian@drelling.com>
2023-02-23 23:10:56 -0600

    Added raw HTTPBody support

--- Sources/KippleNetworking/Foundation/Extensions/URLRequest+Init.swift
+++ Sources/KippleNetworking/Foundation/Extensions/URLRequest+Init.swift
@@ -1,4 +1,4 @@
-// Copyright © 2022 Brian Drelling. All rights reserved.
+// Copyright © 2023 Brian Drelling. All rights reserved.
 
 #if os(iOS) || os(macOS) || os(tvOS) || os(watchOS)
 
@@ -9,6 +9,7 @@ public extension URLRequest {
     /// - Parameter url: The URL for the request. Accepts a URL or a String.
     /// - Parameter method: The HTTP method for the request. Defaults to `GET`.
     /// - Parameter parameters: The parameters to be converted into a String-keyed dictionary to send in the query string or HTTP body.
+    /// - Parameter body: The raw data to be set as the HTTP body of the request. If this value is not nil, then `encoding` must _not_ be `.httpBody`.
     /// - Parameter headers: The HTTP headers to send with the request.
     /// - Parameter encoding: The parameter encoding method. If nil, uses the default encoding for the provided HTTP method.
     /// - Returns: The configured `URLRequest` object.
@@ -16,9 +17,14 @@ public extension URLRequest {
         url: URLConvertible,
         method: HTTPMethod = .get,
         parameters: ParameterDictionaryConvertible? = nil,
+        body: Data? = nil,
         headers: [String: String] = [:],
         encoding: ParameterEncoding? = nil
     ) throws {
+        if case .httpBody = encoding, body?.isEmpty != false {
+            throw NetworkingError.httpBodyConflictsWithParameterEncoding
+        }
+
         let url = try url.asURL()
         self.init(url: url)
 
@@ -28,12 +34,15 @@ public extension URLRequest {
 
         // Parameters must be set after setting headers, because encoding dictates (and therefore overrides) the Content-Type header
         self.setParameters(parameters, method: method, encoding: encoding)
+
+        self.httpBody = body
     }
 
     init(
         url: URLConvertible,
         method: HTTPMethod = .get,
         parameters: ParameterDictionaryConvertible? = nil,
+        body: Data? = nil,
         headers: [HTTPHeader: String],
         encoding: ParameterEncoding? = nil
     ) throws {
@@ -41,6 +50,7 @@ public extension URLRequest {
             url: url,
             method: method,
             parameters: parameters,
+            body: body,
             headers: headers.asHeaderDictionary(),
             encoding: encoding
         )
